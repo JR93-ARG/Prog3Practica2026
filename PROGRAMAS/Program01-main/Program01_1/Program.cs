1d8ba55498947832e54a14436d96d6faac2952ee
JR93-ARG <rodrigosalvador93@gmail.com>
2026-04-07 17:51:04 -0300

    ACTUALIZACION EN CARPETAS DE PROFRAM 01

--- PROGRAMAS/Program01-main/Program01_1/Program.cs
+++ PROGRAMAS/Program01-main/Program01_1/Program.cs
@@ -1,29 +1,28 @@
-﻿namespace Program01;
+﻿namespace Program01_1;
 
 class Program
 {
     static void Main(string[] args)
     {
-       Console.WriteLine("--- SISTEMA DE REGISTRO DE USUARIO ---");
+       Console.WriteLine("--- APTO FISICO ---");
 
-            // 1. Entrada de Texto (Simple)
-            Console.Write("Por favor, ingresa tu nombre: ");
-            string nombre = Console.ReadLine();
+            // 1. Entrada de Texto
+            Console.Write("Por favor, ingresa tu edad: ");
+            int edad = int.Parse(Console.ReadLine());
 
             // 2. Entrada de Números (Requiere Conversión)
-            Console.Write("Ingresa tu edad: ");
-            string edadTexto = Console.ReadLine();
-            int edad = int.Parse(edadTexto); // Convertimos el texto a número entero
+            Console.Write("Ingresa tu nombre: ");
+            string nombre = Console.ReadLine();
 
             // 3. Entrada de Decimales (Precios o Medidas)
-            Console.Write("Ingresa tu estatura (ejemplo: 1,75): ");
-            double estatura = double.Parse(Console.ReadLine()); // Conversión directa en una línea
+            Console.Write("Ingresa se sos amateur (s/n): ");
+            string esAmateur = Console.ReadLine(); // Conversión directa en una línea
 
             // 4. Mostrar los resultados procesados
             Console.WriteLine("\n--- PERFIL CREADO ---");
             Console.WriteLine("Nombre: " + nombre);
-            Console.WriteLine("Edad el próximo año: " + (edad + 1)); // Operación matemática
-            Console.WriteLine("Estatura: " + estatura + " metros");
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Es amateur: " + esAmateur); // Mostrar directamente sin conversión
 
             Console.WriteLine("\nPresiona ENTER para salir.");
             Console.ReadLine();
--- PROGRAMAS/Program01-main/Program01_2/Program.cs
+++ PROGRAMAS/Program01-main/Program01_2/Program.cs
@@ -4,26 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.WriteLine("--- SISTEMA DE REGISTRO DE USUARIO ---");
+       Console.WriteLine("--- SISTEMA DE CONTROL DE USUARIO ---");
 
             // 1. Entrada de Texto (Simple)
-            Console.Write("Por favor, ingresa tu nombre: ");
+            Console.Write("Por favor, ingresa tu apellido: ");
             string nombre = Console.ReadLine();
 
             // 2. Entrada de Números (Requiere Conversión)
-            Console.Write("Ingresa tu edad: ");
-            string edadTexto = Console.ReadLine();
-            int edad = int.Parse(edadTexto); // Convertimos el texto a número entero
+            Console.Write("Ingresa tu area: ");
+            string area = Console.ReadLine();
+           
 
             // 3. Entrada de Decimales (Precios o Medidas)
-            Console.Write("Ingresa tu estatura (ejemplo: 1,75): ");
-            double estatura = double.Parse(Console.ReadLine()); // Conversión directa en una línea
+            Console.Write("Ingresa tu legajo (ejemplo: 12345): ");
+            int legajo = int.Parse(Console.ReadLine()); 
 
+            DateTime actual = DateTime.Now;
+            
+             
             // 4. Mostrar los resultados procesados
-            Console.WriteLine("\n--- PERFIL CREADO ---");
+            Console.WriteLine("\n--- INGRESO REGISTRADO ---");
             Console.WriteLine("Nombre: " + nombre);
-            Console.WriteLine("Edad el próximo año: " + (edad + 1)); // Operación matemática
-            Console.WriteLine("Estatura: " + estatura + " metros");
+            Console.WriteLine("Área: " + area);
+            Console.WriteLine("Legajo: " + legajo);
+            Console.WriteLine("Asistencia del día: " + actual.ToString("dd/MM/yyyy HH:mm:ss"));
 
             Console.WriteLine("\nPresiona ENTER para salir.");
             Console.ReadLine();
